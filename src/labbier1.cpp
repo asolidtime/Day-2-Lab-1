72d65be8cca0e01b162cd88bb25bb6dfb32c417f
Max Leon <maxwellleon55243@gmail.com>
2022-06-28 12:31:55 -0400

    more colors

--- src/labbier1.cpp
+++ src/labbier1.cpp
@@ -6,57 +6,38 @@
 #line 1 "/home/maxwell/CTD-IoT/d2/labbier1/src/labbier1.ino"
 
 
-void doBlinkyThing(int pin, float delayy);
-void doDualBlinkyThing(int pin1, int pin2, float delayy);
-void doWhiteBlinkyThing(float delayy);
+void customColorBlink(int red, int green, int blue, int delayy);
 void doBlinkyThings(float delay);
 void setup();
 void loop();
 #line 3 "/home/maxwell/CTD-IoT/d2/labbier1/src/labbier1.ino"
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(AUTOMATIC);
+int RED = D7;
+int GREEN = D6;
+int BLUE = D5;
 
-void doBlinkyThing(int pin, float delayy) {
+void customColorBlink(int red, int green, int blue, int delayy) {
   for (int i = 0; i < 510; i++) {
-    int temp = i;
-    if (temp > 255) {
-      temp = 510 - temp;
-    }
-    analogWrite(pin, temp);
-    delayMicroseconds((int) (delayy * 1000));
-  }
-}
-void doDualBlinkyThing(int pin1, int pin2, float delayy) {
-  for (int i = 0; i < 510; i++) {
-    int temp = i;
-    if (temp > 255) {
-      temp = 510 - temp;
-    }
-    analogWrite(pin1, temp);
-    analogWrite(pin2, temp);
-    delayMicroseconds((int) (delayy * 1000));
-  }
-}
-void doWhiteBlinkyThing(float delayy) {
-  for (int i = 0; i < 510; i++) {
-    int temp = i;
-    if (temp > 255) {
-      temp = 510 - temp;
-    }
-    analogWrite(D7, temp);
-    analogWrite(D6, temp);
-    analogWrite(D5, temp);
-    delayMicroseconds((int) (delayy * 1000));
+      int temp = i;
+      if (temp > 255) {
+        temp = 510 - temp;
+      }
+      analogWrite(RED, map(temp, 0, 255, 0, red));
+      analogWrite(GREEN, map(temp, 0, 255, 0, green));
+      analogWrite(D5, map(temp, 0, 255, 0, blue));
+      delayMicroseconds((int) (delayy * 1000));
   }
 }
 void doBlinkyThings(float delay) {
-  doBlinkyThing(D7, delay);
-  doDualBlinkyThing(D7, D6, delay);
-  doBlinkyThing(D6, delay);
-  doDualBlinkyThing(D6, D5, delay);
-  doBlinkyThing(D5, delay);
-  doDualBlinkyThing(D5, D7, delay);
-  doWhiteBlinkyThing(delay);
+  customColorBlink(255, 255, 0, delay);
+  customColorBlink(0, 255, 0, delay);
+  customColorBlink(0, 255, 255, delay);
+  customColorBlink(0, 0, 255, delay);
+  customColorBlink(255, 0, 255, delay);
+  customColorBlink(255, 255, 255, delay);
+  customColorBlink(255, 111, 97, delay);
+  customColorBlink(97, 255, 255, delay);
 }
 
 // setup() runs once, when the device is first turned on.
@@ -70,11 +51,11 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  doBlinkyThings(5);
   // for (int i = 0; i < 2; i++) {
   //   doBlinkyThings(1);
   // }
   // for (int i = 0; i < 3; i++) {
   //   doBlinkyThings(0.2);
   // }
+  doBlinkyThings(5);
 }
